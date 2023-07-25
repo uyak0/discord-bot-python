acd0025d9ea98c9ef3f3ee4cf0061fa65a5cd154
uyak0 <milkycoffeethings@gmail.com>
2023-07-25 17:48:14 +0800

    clean up

--- responses.py
+++ responses.py
@@ -1,19 +1,37 @@
 import random
 
 def handle_response(message: str) -> str:       #code for handling responses from user
-
     p_message = message.lower()
-    cmd = p_message[:1]         #separate command from message
-    message = p_message[1:]     #separate message from command
+    cmd_length = 1
+    cmd = p_message[:cmd_length]         #separate command from message
+    message = p_message[cmd_length:]     #separate message from command
 
-    if cmd == '!':          #check for command
-        if message == 'hello':
-            return 'Hey there'
+    if cmd == '-':          #check for command
+        #--ping
+        if message == 'ping':
+            return 'pong :ping_pong:'
         
-        if message == 'roll':
-            return str(random.randint(1, 6))
+        #--roll
+        if message[:4] == 'roll':
+            if message[4:] == '':
+                return str(random.randint(1, 100))
+
+            else:
+                number = int(message[4:])
+                randomnum = random.randint(1, number)
+                return str(randomnum)
         
-        if message == '!help':
-            return "`this is a help message that you can modify`"
-        
+        if message[:4] == 'pick':
+            
+
+
+        #--help
+        if message == 'help':
+            return  "### __List of Commands__\n"+\
+                    "```\n"+\
+                    "1. -ping\n"+\
+                    "2. -roll  <number> (default: 100)\n"+\
+                    "3. -help\n"+\
+                    "```"
+
         return 'I did not understand what your wrote, try again'
